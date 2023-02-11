a7616aeaede24026c02c67cb01a6a5bf89713a06
Luiz Paulo Vargas <luizpaulovargass@gmail.com>
2023-02-10 22:53:10 -0200

    Dia 18
    Jogo mata mosquito

--- curso_web/Javascript/app2/jogo.js
+++ curso_web/Javascript/app2/jogo.js
@@ -1,79 +1,116 @@
+
 var altura = 0
 var largura = 0
 var vidas = 1
+var tempo = 15
 
-function tamanhoJogo() {
-    altura = window.innerHeight
-    largura = window.innerWidth
-    
-    console.log(largura, altura)
+var criaMosquitoTempo = 1500
+
+var nivel = window.location.search
+nivel = nivel.replace('?', '')
+
+if(nivel === 'normal') {
+	//1500
+	criaMosquitoTempo = 1500
+} else if(nivel === 'dificil') {
+	//1000
+	criaMosquitoTempo = 1000
+} else if (nivel === 'chucknorris') {
+	//750
+	criaMosquitoTempo = 750
 }
-tamanhoJogo()
+
+function ajustaTamanhoPalcoJogo() {
+	altura = window.innerHeight
+	largura = window.innerWidth
+
+	console.log(largura, altura)
+}
+
+ajustaTamanhoPalcoJogo()
+
+var cronometro = setInterval(function() {
+
+	tempo -= 1
+
+	if(tempo < 0) {
+		clearInterval(cronometro)
+		clearInterval(criaMosca)
+		window.location.href = 'vitoria.html'
+	} else {
+		document.getElementById('cronometro').innerHTML = tempo
+	}
+	
+}, 1000)
 
 
 function posicaoRandomica() {
-    
-    //remover o mosquito da posição randomica (caso exista)
-    if(document.getElementById('mosquito')) {
-        document.getElementById('mosquito').remove()
 
-        //console.log('v' + vidas)
-        if(vidas > 3) {
-            window.location.href = 'fimdejogo.html'
-        } else {
-            document.getElementById('v' + vidas).src="imagens/coracao_vazio.png"
-            vidas++
-        }    
-    }
-    
 
-    var posicaoX = Math.floor(Math.random() * largura) - 90
-    var posicaoY = Math.floor(Math.random() * altura) - 90
+	//remover o mosquito anterior (caso exista)
+	if(document.getElementById('mosquito')) {
+		document.getElementById('mosquito').remove()
 
-    posicaoX = posicaoX < 0 ? 0 : posicaoX
-    posicaoY = posicaoY < 0 ? 0 : posicaoY
+		//console.log('elemento selecionado foi: v' + vidas)
+		if(vidas > 3) {
 
-    console.log(posicaoX, posicaoY)
+			window.location.href = 'fim_de_jogo.html'
+		} else {
+			document.getElementById('v' + vidas).src = "imagens/coracao_vazio.png"
 
-    //Criar o elemento HTML
-    var mosquito = document.createElement('img')
-    mosquito.src = 'imagens/mosca.png'
-    mosquito.className = tamanhoAleatorio() + ' ' + ladoAleatorio()
-    mosquito.style.left = posicaoX + 'px'
-    mosquito.style.top = posicaoY + 'px'
-    mosquito.style.position = 'absolute'
-    mosquito.id = 'mosquito'
-    mosquito.onclick = function() {
-        this.remove()
-    }
+			vidas++
+		}
+	}
 
-    document.body.appendChild(mosquito)
+	var posicaoX = Math.floor(Math.random() * largura) - 90
+	var posicaoY = Math.floor(Math.random() * altura) - 90
 
-    console.log(ladoAleatorio())
+	posicaoX = posicaoX < 0 ? 0 : posicaoX
+	posicaoY = posicaoY < 0 ? 0 : posicaoY
+
+	console.log(posicaoX, posicaoY)
+
+	//criar o elemento html
+	var mosquito = document.createElement('img')
+	mosquito.src = 'imagens/mosquito.png'
+	mosquito.className = tamanhoAleatorio() + ' ' + ladoAleatorio()
+	mosquito.style.left = posicaoX + 'px'
+	mosquito.style.top = posicaoY + 'px'
+	mosquito.style.position = 'absolute'
+	mosquito.id = 'mosquito'
+	mosquito.onclick = function() {
+		this.remove()
+	}
+
+	document.body.appendChild(mosquito)
 
 }
 
 function tamanhoAleatorio() {
-    var classe = Math.floor(Math.random() * 3)
+	var classe = Math.floor(Math.random() * 3)
+	
+	switch(classe) {
+		case 0:
+			return 'mosquito1'
+		
+		case 1:
+			return 'mosquito2'
 
-    switch(classe) {
-        case 0:
-            return 'mosquito1'
-        case 1:
-            return 'mosquito2'
-        case 2:
-            return 'mosquito3'
-    }            
-}  
+		case 2:
+			return 'mosquito3'
+	}
+}
 
 function ladoAleatorio() {
-    var classe = Math.floor(Math.random() * 2)
+	var classe = Math.floor(Math.random() * 2)
+	
+	switch(classe) {
+		case 0:
+			return 'ladoA'
+		
+		case 1:
+			return 'ladoB'
 
-    switch(classe) {
-        case 0:
-            return 'ladoA'
-        case 1:
-            return 'ladoB'
-       
-    }            
-}  
+	}
+}
+
